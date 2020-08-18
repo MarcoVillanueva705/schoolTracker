bbb75cf1e100d80d5f9fc6d97b6a95cb56e3d417
marco villanueva <villanuevamarco98@gmail.com>
2020-08-17 20:35:32 -0600

    Add project files.

---
+++ schoolTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace schoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentGrades = new int[] { 77, 88, 98, 76, 82 };
+            Console.WriteLine(studentGrades[2]);
+        }
+    }
+}
