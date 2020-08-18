589fdc2cb41279a2941f15aabbc827dafb942f56
marco villanueva <villanuevamarco98@gmail.com>
2020-08-17 20:39:09 -0600

    foreach loop

--- schoolTracker/Program.cs
+++ schoolTracker/Program.cs
@@ -7,7 +7,11 @@ namespace schoolTracker
         static void Main(string[] args)
         {
             var studentGrades = new int[] { 77, 88, 98, 76, 82 };
-            Console.WriteLine(studentGrades[2]);
+            foreach (var grade in studentGrades)
+            {
+
+            Console.WriteLine(grade);
+            }
         }
     }
 }
